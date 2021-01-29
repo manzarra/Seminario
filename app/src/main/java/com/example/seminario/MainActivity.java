ba1e53c738da016f2c62dd24622a5980385e82cb
André Pedro <32388352+manzarra@users.noreply.github.com>
2021-01-29 22:29:16 +0000

    atualizacao

--- app/src/main/java/com/example/seminario/MainActivity.java
+++ app/src/main/java/com/example/seminario/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.seminario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void linear(View view) {
+        setContentView(R.layout.linearlayout);
+    }
+
+    public void bitmap(View view) {
+        setContentView(R.layout.ninebitmaps);
+    }
+
+    public void vetores(View view) {
+        setContentView(R.layout.vetores);
+    }
+
+    public void global(View view) {
+        setContentView(R.layout.globais);
+    }
+
+    public void voltar(View view) {
         setContentView(R.layout.activity_main);
     }
 }
